6a65a9a17058f8a71c1b7cd0b421ef0b008a66a2
MEUNIER Sébastien <stg3005@brufor.lan>
2024-07-09 15:55:05 +0200

    proj explosion

--- Assets/#Project/Scripts/Projectile.cs
+++ Assets/#Project/Scripts/Projectile.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Projectile : MonoBehaviour
 {
     public ProjectilePool pool;
+    public GameObject explosion;
     public bool initialized = false;
     [HideInInspector]public UnityEvent<int> onHit;
     // Start is called before the first frame update
@@ -25,6 +27,11 @@ public class Projectile : MonoBehaviour
     }
     void OnTriggerEnter(Collider other){
         //todo: other collider behavior
+        //* 1) play explosion on impact
+        GameObject explo = Instantiate(explosion, transform.position, transform.rotation);
+        explo.GetComponent<ParticleSystem>().Play();
+        Destroy(explo, 1f);
+        //* 2) kill & respawn if player or bot
         //this.Die();
         Destroy(gameObject);
     }
